da7b6981ff8d0b4a8d9ef7b8c3bc91dcf6358bca
Frico Simon <simonfriko@gmail.com>
2024-01-30 08:56:23 +0700

    solve Opposite number question - 8 kyu

---
+++ 8 kyu/oppositeNumber.js
@@ -0,0 +1,13 @@
+// Opposite number
+
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// my solution
+function opposite(number) {
+    return number * -1;
+}
+
+// best practice
+function opposite(number) {
+    return (-number);
+}
