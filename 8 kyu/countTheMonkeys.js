2119c5ffcb01de4fd66288504d6c075a7915a39c
Frico Simon <simonfriko@gmail.com>
2024-01-31 09:49:41 +0700

    solve Count the Monkeys question - 8 kyu

---
+++ 8 kyu/countTheMonkeys.js
@@ -0,0 +1,12 @@
+// Count the Monkeys!
+
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+// my solution
+function monkeyCount(n) {
+    let result = []
+    for (let i = 1; i <= n; i++) {
+        result.push(i)
+    }
+    return result
+}
