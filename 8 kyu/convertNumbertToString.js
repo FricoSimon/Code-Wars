c5b7decb99bcf384dec637b02bd56d4e775a6754
Frico Simon <simonfriko@gmail.com>
2024-01-31 09:55:42 +0700

    solve Convert a Number to a String question - 8 kyu

---
+++ 8 kyu/convertNumbertToString.js
@@ -0,0 +1,15 @@
+// Convert a Number to a String!
+
+//  https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// my solution
+function numberToString(num) {
+    const convert = String(num);
+
+    return convert;
+}
+
+// best practice
+function numberToString(num) {
+    return num.toString();
+}
