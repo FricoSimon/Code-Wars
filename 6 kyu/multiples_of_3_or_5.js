51a4ff3e2725f88024094f321b7629d730e133fd
Frico Simon <simonfriko@gmail.com>
2024-02-07 14:18:02 +0700

    solve Multiples of 3 or 5 question - 6 kyu

---
+++ 6 kyu/multiples_of_3_or_5.js
@@ -0,0 +1,17 @@
+// Multiples of 3 or 5
+
+// https://www.codewars.com/kata/514b92a657cdc65150000006
+
+// my solution
+function solution(number) {
+    var sum = 0;
+
+    for (var i = 1; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
+    }
+    return sum;
+}
+
+// best practice
