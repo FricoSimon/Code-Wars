583dfbd14c2d5287db0870a7dc702a3e3d34c5bf
Frico Simon <simonfriko@gmail.com>
2024-02-19 11:24:59 +0700

    solve Will you make it? question  - 8 kyu

---
+++ 8 kyu/willYouMakeIt.js
@@ -0,0 +1,13 @@
+// Will you make it?
+
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+// my solution
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump / mpg <= fuelLeft
+};
+
+// best practice
+const zeroFuel2 = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump / mpg <= fuelLeft
+};
