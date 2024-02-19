6d53369609168e9f1ca9aa8d90af39b6ea5b9867
Frico Simon <simonfriko@gmail.com>
2024-02-19 11:15:59 +0700

    solve Sentence Smash question - 8 kyu

---
+++ 8 kyu/sentenceSmash.js
@@ -0,0 +1,13 @@
+// Sentence Smash
+
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// my solution
+function smash(words) {
+    return words.join(" ");
+};
+
+// best practice
+smash = function (words) {
+    return words.join(" ");
+};
